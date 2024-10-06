9334d27eaf25fa138147047b14132969bb1e25e3
gachafrejus <frejuskinf@gmail.com>
2024-10-06 13:27:45 -0700

    task 0

--- 0-pascal_triangle.py/0-main.py
+++ 0-pascal_triangle.py/0-main.py
@@ -1,25 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-This module contains a function that prints Pascal's Triangle.
-
-Pascal's Triangle is a triangular array of numbers where each
-row is constructed by adding the two numbers above it to the left
-and right. The outermost numbers of each row are always 1.
+0-main
 """
-
 pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
-
 
 def print_triangle(triangle):
     """
-    Print the triangle.
-
-    Args:
-        triangle (list): A list of lists representing
-        the rows of Pascal's Triangle.
-
-    Returns:
-        None
+    Print the triangle
     """
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
--- 0-pascal_triangle.py/0-pascal_triangle.py
+++ 0-pascal_triangle.py/0-pascal_triangle.py
@@ -1,25 +1,15 @@
-#!/usr/bin/python3
-"""
-returns a list of lists of integers representing the Pascal's triangle of n
-"""
-
-
+# 0-pascal_triangle.py
 def pascal_triangle(n):
-    """
-    returns an empty list if n <= 0
-    """
-
-    if(n <= 0):
+    if n <= 0:
         return []
-    else:
-        a = [[] for i in range(n)]
-        for i in range(n):
-            for j in range(i+1):
-                if(j < i):
-                    if(j == 0):
-                        a[i].append(1)
-                    else:
-                        a[i].append(a[i-1][j]+a[i-1][j-1])
-                elif(j == i):
-                    a[i].append(1)
-        return a
+    
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+    
+    return triangle
